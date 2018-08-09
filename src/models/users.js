01686cc68f3885bf42ccdacc2767026dd00a6077
Larrymemo <larrymemo9779@gmail.com>
2018-08-09 11:10:21 -0500

    integration GraphQl

--- src/models/users.js
+++ src/models/users.js
@@ -26,7 +26,7 @@ const UserSchema = new Schema({
 
 UserSchema.pre('save',function(next){
     let user = this;
-    if (!user.isModified('password')){return next();}
+    if (!user.isModified('password')){return next()}
     bcrypt.genSalt(SALT,(err,salt)=>{
         if (err) return next(err);
         bcrypt.hash(user.password,salt,(err,hash)=>{
